07294f7be46a758ad4575dd77a5d2f7c14a3c323
Logan Gall <141678252+logan-gall@users.noreply.github.com>
2025-02-05 02:28:39 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ ifdef GL_TEST
 endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_spatial_processing_unit user_project (
 
       // Include power ports for the Gate Level test:
 ifdef GL_TEST
